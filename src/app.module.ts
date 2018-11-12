5c4276c8b424d3050b90ad26537aea31b6c2a152
Almeida <diogo.santos.almeida@accenture.com>
2018-11-12 18:19:20 -0200

    create schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TimelineModule } from './timeline/timeline.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [TimelineModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TimelineModule,
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      debug: true,
+      include: [TimelineModule]
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
